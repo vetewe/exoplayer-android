880159e058cb4a82e03130e995819f12b3fdcfd7
vtw <vtiaraw2@gmail.com>
2024-11-20 11:55:19 +0700

    refactor: adding detail to the notification

--- app/src/main/java/com/example/myexoplayer/PlaybackService.kt
+++ app/src/main/java/com/example/myexoplayer/PlaybackService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
@@ -18,14 +19,31 @@ class PlaybackService : MediaSessionService() {
     }
 
     private fun initializeSessionAndPlayer() {
-        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+        val videoItem = MediaItem.Builder()
+            .setUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setTitle("Bersama Dicoding, Kembangkan Dirimu Menjadi Talenta Digital Berstandar Global")
+                    .setArtist("Dicoding")
+                    .build()
+            ).build()
+        val audioItem = MediaItem.Builder()
+            .setUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setTitle("Ukulele")
+                    .setArtist("Bensound")
+                    .build()
+            ).build()
+
         val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
             exoPlayer.setMediaItem(videoItem)
             exoPlayer.addMediaItem(audioItem)
             exoPlayer.prepare()
         }
+
         mediaSession = MediaSession.Builder(this, player).build()
+
     }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
